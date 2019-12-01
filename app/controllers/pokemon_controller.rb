b8378a1b2ce600675be81ece573e7d091b071193
shogo-hr <harada.tosho.2016@gmail.com>
2019-12-01 17:37:59 +0900

    ポケモンを検索して表示できるようにした

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,4 +1,10 @@
 class PokemonController < ApplicationController
   def new
+    search_params = params[:search]
+    response = Faraday.get "https://pokeapi.co/api/v2/pokemon/#{search_params}"
+
+    if response.status == 200
+      @results = JSON.parse(response.body)
+    end
   end
 end
