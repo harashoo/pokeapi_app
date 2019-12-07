eab2a5a23809b8ef52c53d7e8579647fa234a19c
shogo-hr <harada.tosho.2016@gmail.com>
2019-12-07 13:33:04 +0900

    create実装

---
+++ app/controllers/pokemons_controller.rb
@@ -0,0 +1,32 @@
+class PokemonsController < ApplicationController
+  def index
+    @pokemons = Pokemon.all
+  end
+
+  def new
+    return if params[:search].blank?
+    client = ApiClient.new
+    response = client.get_pokemon(params[:search])
+    @pokemon = Pokemon.new(order: response.id, name: response.name, image_url: response.sprites.front_default)
+  end
+
+  def show
+    @pokemon = Pokemon.find(params[:id])
+  end
+
+  def create
+    @pokemon = Pokemon.new(pokemon_params)
+
+    if @pokemon.save
+      redirect_to pokemons_path, notice: "「#{@pokemon.name}」をゲットしました。"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def pokemon_params
+    params.require(:pokemon).permit(:order, :name, :image_url)
+  end
+end
